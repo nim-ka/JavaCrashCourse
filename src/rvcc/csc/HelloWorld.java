bb847f6e5d286dee0e1ca7a89f29fecf6a435644
kennycastro007 <kennycastro007@gmail.com>
2019-09-30 12:29:04 -0400

    Changed ben to people in hello world

--- src/rvcc/csc/HelloWorld.java
+++ src/rvcc/csc/HelloWorld.java
@@ -3,6 +3,6 @@ package rvcc.csc;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Ben!");
+        System.out.println("Hello people!");
     }
 }
